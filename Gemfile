9308e2757a0ed8372d48c55e2b7b36688af95f10
John Sawers <john@johnksawers.com>
2011-06-28 08:47:51 -0400

    cleaning up the stuff from before rails 3.1

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 gem 'rails', '3.1.0.rc4'
-gem 'sqlite3'
+gem 'mysql'
 gem 'sass-rails', "~> 3.1.0.rc"
 gem 'coffee-script'
 gem 'uglifier'
--- db/migrate/20110628014846_remove_response_from_question.rb
+++
@@ -1,9 +0,0 @@
-class RemoveResponseFromQuestion < ActiveRecord::Migration
-  def self.up
-    remove_column :questions, :response
-  end
-
-  def self.down
-    add_column :questions, :response, :integer
-  end
-end
