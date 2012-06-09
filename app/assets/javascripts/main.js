1ba55b5cd4ab0403ac6137af830b19fce3514d5b
John <john@johnksawers.com>
2012-06-09 00:26:49 -0400

    Clean up the dynamic form render-via-javascript so it's all specific to the question model, rather than being generic

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+function remove_questions(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
+
+function add_questions(link, content) {
+  //var new_id = new Date().getTime();
+  //var regexp = new RegExp("new_question", "g")
+  //$(link).parent().before(content.replace(regexp, new_id));
+$(link).parent().before(content);
+}
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def link_to_remove_questions(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_questions(this)")
+  end
+  
+  def link_to_add_questions(name, f)
+    new_object = f.object.class.reflect_on_association(:questions).klass.new
+    fields = f.fields_for(:questions, new_object, :child_index => "new_question") do |builder|
+      render("question_fields", :f => builder)
+    end
+    link_to_function(name, "add_questions(this, \"#{escape_javascript(fields)}\")")
+  end
 end
+
