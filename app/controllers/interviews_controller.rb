033566710ee15f5145a1019aebaf6e172429f8d8
John <john@johnksawers.com>
2012-06-16 16:37:48 -0400

    Sorted out proper whitespace settings, reformatted code according to that

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,83 +1,83 @@
 class InterviewsController < ApplicationController
-  # GET /interviews
-  # GET /interviews.json
-  def index
-    @interviews = Interview.all
+    # GET /interviews
+    # GET /interviews.json
+    def index
+        @interviews = Interview.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @interviews }
+        respond_to do |format|
+            format.html # index.html.erb
+            format.json { render json: @interviews }
+        end
     end
-  end
 
-  # GET /interviews/1
-  # GET /interviews/1.json
-  def show
-    @interview = Interview.find(params[:id])
+    # GET /interviews/1
+    # GET /interviews/1.json
+    def show
+        @interview = Interview.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @interview }
+        respond_to do |format|
+            format.html # show.html.erb
+            format.json { render json: @interview }
+        end
     end
-  end
 
-  # GET /interviews/new
-  # GET /interviews/new.json
-  def new
-    @interview = current_user.interviews.build
+    # GET /interviews/new
+    # GET /interviews/new.json
+    def new
+        @interview = current_user.interviews.build
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @interview }
+        respond_to do |format|
+            format.html # new.html.erb
+            format.json { render json: @interview }
+        end
     end
-  end
 
-  # GET /interviews/1/edit
-  def edit
-    @interview = Interview.find(params[:id])
-  end
-
-  # POST /interviews
-  # POST /interviews.json
-  def create
-    @interview = current_user.interviews.build(params[:interview])
-
-    respond_to do |format|
-      if @interview.save
-        format.html { redirect_to @interview, notice: 'Interview was successfully created.' }
-        format.json { render json: @interview, status: :created, location: @interview }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @interview.errors, status: :unprocessable_entity }
-      end
+    # GET /interviews/1/edit
+    def edit
+        @interview = Interview.find(params[:id])
     end
-  end
 
-  # PUT /interviews/1
-  # PUT /interviews/1.json
-  def update
-    @interview = Interview.find(params[:id])
+    # POST /interviews
+    # POST /interviews.json
+    def create
+        @interview = current_user.interviews.build(params[:interview])
 
-    respond_to do |format|
-      if @interview.update_attributes(params[:interview])
-        format.html { redirect_to @interview, notice: 'Interview was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit",notice: 'There was an error updating' }
-        format.json { render json: @interview.errors, status: :unprocessable_entity }
-      end
+        respond_to do |format|
+            if @interview.save
+                format.html { redirect_to @interview, notice: 'Interview was successfully created.' }
+                format.json { render json: @interview, status: :created, location: @interview }
+            else
+                format.html { render action: "new" }
+                format.json { render json: @interview.errors, status: :unprocessable_entity }
+            end
+        end
     end
-  end
 
-  # DELETE /interviews/1
-  # DELETE /interviews/1.json
-  def destroy
-    @interview = Interview.find(params[:id])
-    @interview.destroy
+    # PUT /interviews/1
+    # PUT /interviews/1.json
+    def update
+        @interview = Interview.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to root_url,notice: "Interview #{@interview.name} deleted" }
-      format.json { head :ok }
+        respond_to do |format|
+            if @interview.update_attributes(params[:interview])
+                format.html { redirect_to @interview, notice: 'Interview was successfully updated.' }
+                format.json { head :ok }
+            else
+                format.html { render action: "edit", notice: 'There was an error updating' }
+                format.json { render json: @interview.errors, status: :unprocessable_entity }
+            end
+        end
     end
-  end
+
+    # DELETE /interviews/1
+    # DELETE /interviews/1.json
+    def destroy
+        @interview = Interview.find(params[:id])
+        @interview.destroy
+
+        respond_to do |format|
+            format.html { redirect_to root_url, notice: "Interview #{@interview.name} deleted" }
+            format.json { head :ok }
+        end
+    end
 end
