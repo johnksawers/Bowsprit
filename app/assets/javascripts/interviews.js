28068b355c05e3bc1930fb8799623648f98eeb82
John <john@johnksawers.com>
2012-06-16 19:27:54 -0400

    Refactor to remove calls to link_to_function which is deprecated in Rails 3.2.5
    This makes the javascript much less obtrustive, though more work remains to ensure all the buttons and links function as links without JS

--- app/assets/javascripts/interviews.js
+++ app/assets/javascripts/interviews.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
-	$(".edit_button").mouseup(function () {
+	$(".edit_button").click(function () {
 		show_edit(this);
 	});
+	$('#add_question_button').mousedown(function(){
+		add_question_fields();
+	})
+	$('.remove_question_button').mousedown(function(){
+		remove_question(this);
+	})
 });
 function show_edit(link) {
 	edit_fields = $(link).parent().find('.edit_fields');
@@ -10,4 +16,15 @@ function show_edit(link) {
 	} else {
 		edit_fields.addClass("hidden");
 	}
+}
+function add_question_fields(){
+	var new_id = new Date().getTime();
+	var regexp = new RegExp("new_question", "g")
+	new_fields_blob = dynamic_question_form;
+	$("#new_question_fields_container").html(new_fields_blob.replace(regexp, new_id));
+}
+
+function remove_question(link) {
+	$(link).prev("input[type=hidden]").val("1");
+	$(link).closest("li").hide();
 }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
     def link_to_remove_questions(name, f)
-        f.hidden_field(:_destroy) + link_to_function(name, "remove_questions(this)")
+        f.hidden_field(:_destroy) + link_to('Remove','#', :class => 'remove_question_button')
     end
 
-    def link_to_add_questions(name, f)
+    def build_dynamic_question_form(f)
         new_object = f.object.class.reflect_on_association(:questions).klass.new
-        fields     = f.fields_for(:questions, new_object, :child_index => "new_question") do |builder|
+        fields = f.fields_for(:questions, new_object, :child_index => "new_question") do |builder|
             render("question_fields", :f => builder)
         end
-        link_to_function(name, "add_questions(this, \"#{escape_javascript(fields)}\")", :class => 'btn btn-primary')
+        escape_javascript(fields)
     end
 end
 
