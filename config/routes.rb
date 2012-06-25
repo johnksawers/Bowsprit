ec011b58e04ea78796b82a657f9e1a9854d27e97
John <john@johnksawers.com>
2012-06-24 22:51:55 -0400

    Add pricing, FAQ about pages, and update navigation to point to them.
    Also update the navigation so tha tit highlights the current nav item

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,24 @@ Repartee::Application.routes.draw do
     end
     root :to => 'home#index'
     devise_for :users
+    devise_scope :user do
+        get 'sign_in', :to => 'users#sign_in', :as => 'sign_in'
+        get 'sign_up', :to => 'devise/registration#new', :as => 'sign_up'
+        get 'sign_out', :to => 'devise/sessions#destroy', :as => 'sign_out'
+    end
     resources :users, :only => [:show, :index]
 
     resources :question_sets do
         resources :interviews
     end
-    resources :answers
+    resources :interviews do
+        resources :answers
+    end
     resources :questions
 
-    #match 'question_sets/:id/conduct_interview', :to => 'interviews#new', :as => 'conduct_interview'
+    get '/pricing', :to => 'home#pricing', :as => 'pricing'
+    get '/faq', :to => 'home#faq', :as => 'faq'
+    get '/about', :to => 'home#about', :as => 'about'
+    #match 'question_sets/:id/conduct_interview', :to => 'interviews#conduct', :as => 'conduct_interview'
 
 end
