6bbd2bbe31bce03961dc44c60b542d8d2a8a925d
John <john@johnksawers.com>
2012-06-28 23:49:14 -0400

    Update email configs for dev and prod to use the SSL port for Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Repartee::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.default :charset => "utf-8"
 
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
-      :port      => 25,
+      :port      => 587,
       :user_name => ENV["MANDRILL_USERNAME"],
       :password  => ENV["MANDRILL_API_KEY"]
     }
