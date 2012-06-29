7c190392d90c594aa9e8b6645b7825f08f0b9eb9
John <john@johnksawers.com>
2012-06-28 22:07:00 -0400

    Update path where you're sent after saving user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, :alert => exception.message
-  end
+    protect_from_forgery
+    rescue_from CanCan::AccessDenied do |exception|
+        redirect_to root_path, :alert => exception.message
+    end
+    protected
+
+    def after_update_path_for(resource)
+        root_path
+    end
 end
