4a315a584ef6bd031bf68689bfd83f083144819b
John <john@johnksawers.com>
2012-09-12 20:51:25 -0400

    Add config change so asset precompilation won't happen on Heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Repartee
     config.generators do |g|
       g.view_specs false
       g.helper_specs false
-      
+
     end
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -33,7 +33,7 @@ module Repartee
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
-
+    config.assets.initialize_on_precompile = false
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
